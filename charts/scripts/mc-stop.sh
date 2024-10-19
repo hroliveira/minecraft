78f6c93b9f9591c457af399a9c2afe4bcff6ce5d
Helinton Oliveira <helintonoliveira@hro.dev.br>
2024-10-19 17:23:29 -0300

    fix: backup teste

--- charts/scripts/mc-stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Script para parar o servidor Minecraft
-screen -S minecraft -p 0 -X stuff "stop$(printf \\r)"
-
-# Aguardar o servidor parar completamente
-while screen -list | grep -q minecraft; do
-  echo "Aguardando o servidor Minecraft desligar..."
-  sleep 5
-done
