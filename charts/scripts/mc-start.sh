090c52c85022d48e840cc6fdb76e4d693884135b
Helinton Oliveira <helintonoliveira@hro.dev.br>
2024-10-19 17:23:29 -0300

    accessModes:     - ReadWriteMany

--- charts/scripts/mc-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script para iniciar o servidor Minecraft
-
-# Navegar até o diretório do servidor Minecraft
-cd /data || exit
-
-# Iniciar o servidor Minecraft em uma tela separada
-screen -dmS minecraft java -Xmx2G -Xms1G -jar server.jar nogui
